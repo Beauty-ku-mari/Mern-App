39a64e4c1e26d5c4a24e3d37c4a784e1371e966f
Beauty kumari <kbeauty193@gmail.com>
2024-09-04 02:47:40 +0530

    'add'

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const Student = require('../models/studentModel');
+
+// Get all students
+exports.getAllStudents = async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Create a new student
+exports.createStudent = async (req, res) => {
+  const student = new Student(req.body);
+  try {
+    const newStudent = await student.save();
+    res.status(201).json(newStudent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Additional CRUD operations can be added similarly
