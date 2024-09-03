ec6fcd135c5a5ecb56640b0b3fb4967e66e6b195
Beauty kumari <kbeauty193@gmail.com>
2024-09-04 03:31:48 +0530

    'commit'

---
+++ frontend/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import StudentList from '../components/StudentList';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Student Management System</h1>
+    <StudentList />
+  </div>
+);
+
+export default Home;
